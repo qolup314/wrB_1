79ca26a2c23565c051e00e91261943c703e46340
qolup314 <qolup314@gmail.com>
2024-03-09 09:04:19 +0900

    commit word_recognition_Brownian20240309_1

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'word recognition from Brownian motion letters' });
+});
+
+module.exports = router;
